b065b315fab1e213c42f2cab00014de7ba9e90a8
unknown <firdhair30@gmail.com>
2023-01-04 00:59:53 +0700

    created menu component using tailwind

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
-//import './App.css';
 import HelloWorld from './Components/HelloWorld';
-import CounterExample from './Components/CounterExample';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
       <HelloWorld name="Fira"/>
-      <CounterExample/>
+      <Footer/>
     </div>
   );
 }
